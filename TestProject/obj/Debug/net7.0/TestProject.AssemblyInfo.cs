9cb33434ac40b4cef42f89942b8dfbc0862ec8f1
Brashovyan Artem <artembrash@mail.ru>
2023-11-23 17:13:53 +0500

    спецаильно ошибка 2

--- TestProject/obj/Debug/net7.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net7.0/TestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb55c4e446f9d725367c05db2b455301d3a7b6a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ddd1765d91ac4d481ebd91890c25a1e41b7c220")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
